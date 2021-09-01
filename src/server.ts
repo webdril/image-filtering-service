14df7dd38417c6d558169e626c9cb10758a5fbf1
webdril <jimbahamas03@yahoo.com>
2021-08-31 23:02:12 -0500

    made changes to the build file

--- src/server.ts
+++ src/server.ts
@@ -13,46 +13,27 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
   app.get("/filteredimage/", async (req: Request, res: Response) => {
     let { image_url } = req.query;
 
     if (!image_url) {
-      return res.status(400).send(`image_url is required`);
+      return res.status(400).send(`image_url needed`);
     }
 
     try {
       const filteredpath = await filterImageFromURL(image_url)
 
       await res.status(200).sendFile(filteredpath, {}, (err) => {
-        if (err) { return res.status(422).send(`Not able to process the image`); }
-        // Deleting the used image file.
+        if (err) { return res.status(422).send(`image cannot be processed`); }
+       
         deleteLocalFiles([filteredpath])
       })
     } catch (err) {
-      res.status(422).send(`Not able to process the image, Make sure image url is correct`)
+      res.status(422).send(`image cannot be processed, please ensure image url is correct`)
     }
   });
 
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
+ 
   app.get( "/", async ( req, res ) => {
     res.status(200).send("try GET /filteredimage?image_url={{}}")
   });
